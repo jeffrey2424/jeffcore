de8bf072f7f3c8ad458dbdb4065ec3ce883ae44f
Jeff <jeffrey.t.sharpe@gmail.com>
2022-12-28 12:22:04 +0000

    reader itteration2

--- reader.asm
+++ reader.asm
@@ -8,8 +8,42 @@ _start:
 	int 0x80
 
 func:
+	push ebp
+	mov ebp, esp
+
+	; Open file
+	mov eax, 5
+	mov ebx, file_name
+	mov ecx, 0
+	mov edx, 0777
+	int 0x80
+
+	mov [fd_in], eax
+
+	; Read file
+	mov eax, 3
+	mov ebx, [fd_in]
+	mov ecx, info
+	mov edx, 24
+
+	; Print to stdout
+	mov eax, 4
+	mov ebx, 1
+	mov ecx, info
+	mov edx, 24
+	int 0x80
+
+	mov esp, ebp
+	pop ebp
+	ret
 
 
 section	.data
-file_name db ''
+file_name db '/home/jeffrey_t_sharpe/jeffcore_project/jeffcore/base_calc.jeff'
+len 24
+
+section .bss
+fd_out resb 1
+fd_in  resb 1
+info resb  24
 	
