3328d7248af4d04b27d8f3397b9091bd55e901cb
Jeff Sharpe <jeffrey.t.sharpe@gmail.com>
2022-12-28 17:40:57 +0000

    iteration 1

--- reader.asm
+++ reader.asm
@@ -1,38 +1,47 @@
+SYS_EXIT  equ 1
+SYS_READ  equ 3
+SYS_WRITE equ 4
+SYS_OPEN  equ 5
+STDIN     equ 0
+STDOUT    equ 1
+
+SYS_CALL  equ 0x80
+
 section	.text
    global _start         ;must be declared for using gcc
 	
 _start:
 	call func
-	mov eax, 1
+	mov eax, SYS_EXIT
 	mov ebx, 0
-	int 0x80
+	int SYS_CALL
 
 func:
 	push ebp
 	mov ebp, esp
 
 	; Open file
-	mov eax, 5
+	mov eax, SYS_OPEN
 	mov ebx, file_name
 	mov ecx, 0
 	mov edx, 0777
-	int 0x80
+	int SYS_CALL
 
 	mov [fd_in], eax
 
 	; Read file
-	mov eax, 3
+	mov eax, SYS_READ
 	mov ebx, [fd_in]
 	mov ecx, info
 	mov edx, 999
-	int 0x80
+	int SYS_CALL
 
 	; Print to stdout
-	mov eax, 4
+	mov eax, SYS_WRITE
 	mov ebx, 1
 	mov ecx, info
 	mov edx, 999
-	int 0x80
+	int SYS_CALL
 
 	mov esp, ebp
 	pop ebp
@@ -40,7 +49,7 @@ func:
 
 
 section	.data
-file_name db '/home/jeffrey_t_sharpe/jeffcore_project/jeffcore/base_calc.jeff'
+file_name db 'base_calc.jeff'
 
 
 section .bss
